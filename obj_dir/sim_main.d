f66b195fc9ec61f142f73171b3f63a2dc5f13797
Yingjie Tang <chnjstyj@outlook.com>
2025-01-07 23:51:21 +0800

    remove obj_dir

--- obj_dir/sim_main.d
+++
@@ -1,8 +0,0 @@
-sim_main.o: ../csrc/sim_main.cpp \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h VTOP.h \
- /usr/local/share/verilator/include/verilated_save.h \
- /usr/local/share/verilator/include/verilated.h
